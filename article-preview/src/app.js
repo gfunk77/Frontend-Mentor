6be3e1bae49a332f303bd400cad30d81003d7fc5
gfunk77 <faraci77@gmail.com>
2024-04-28 17:25:56 -0400

    media and click func

--- article-preview/src/app.js
+++ article-preview/src/app.js
@@ -1,1 +1,15 @@
 import '@fortawesome/fontawesome-free/css/all.css';
+
+const cardFooter = document.querySelector('.card-footer');
+const info = document.querySelector('.info');
+const icons = document.querySelector('.icons');
+const shareIcon = document.querySelectorAll('.share-icon');
+
+function toggleClasses() {
+  cardFooter.classList.toggle('active');
+  info.classList.toggle('hidden');
+  icons.classList.toggle('hidden');
+}
+shareIcon.forEach((icon) => {
+  icon.addEventListener('click', toggleClasses);
+});
